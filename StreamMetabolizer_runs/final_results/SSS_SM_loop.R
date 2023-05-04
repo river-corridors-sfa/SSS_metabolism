30058cf55967a0067c005a2879f2eca22dcd3f28
mhkaufmanpnnl <52215289+mhkaufmanpnnl@users.noreply.github.com>
2023-05-04 13:13:51 -0400

    preparing for final stream metabolizer data runs with published data

--- StreamMetabolizer_runs/final_results/SSS_SM_loop.R
+++ StreamMetabolizer_runs/final_results/SSS_SM_loop.R
@@ -6,7 +6,9 @@ metadata.path="C:/Users/kauf093/OneDrive - PNNL/Documents/GitHub/SSS_metabolism/
 
 output.path="C:/Users/kauf093/OneDrive - PNNL/Documents/GitHub/SSS_metabolism/StreamMetabolizer_runs/final_results/"
 setwd(output.path)
-metadata=read.csv(paste(metadata.path,'SSS_Metadata_IGSN-Mapping.csv',sep=''),skip=1,header=T)
+metadata=read.csv(paste(metadata.path,'SSS_Metadata_IGSN-Mapping_FAKE.csv',sep=''),skip=1,header=T)
+metadata <- metadata[grepl("Water", metadata$Sample_Name, ignore.case = TRUE),]
+
 SITE_LIST<-data.frame(substring(metadata$Sample_Name,0,6),metadata$Locality)
 colnames(SITE_LIST)<-c('PARENT_ID','SITE_ID')
 for(i in 1:length(SITE_LIST[,1])) { 
@@ -15,12 +17,12 @@ for(i in 1:length(SITE_LIST[,1])) {
 
 PARENT_ID<-SITE_LIST[i,1]
 SITE_ID<-SITE_LIST[i,2]
-htmlfilename=paste(output.path,PARENT_ID,'_',SITE_ID,'_SM_.HTML',sep="")
+htmlfilename=paste(output.path,PARENT_ID,'_',SITE_ID,'_SM_output.HTML',sep="")
   
-rmarkdown::render("smet_final_template.Rmd",
+rmarkdown::render("SSS_SM_final_template.Rmd",
                   output_file = htmlfilename,
                   params = list(
-                    PARENT_ID=PARENT_ID
+                    PARENT_ID=PARENT_ID,
                     SITE_ID=SITE_ID
 ))
 
